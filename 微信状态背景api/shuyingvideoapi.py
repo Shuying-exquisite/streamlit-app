e32e77c836a781a6901c289b1d56ec9af1ee2076
ShuyinYang <103890056+Shuying-exquisite@users.noreply.github.com>
2024-11-16 01:55:52 +0800

    Update shuyingvideoapi.py

--- 微信状态背景api/shuyingvideoapi.py
+++ 微信状态背景api/shuyingvideoapi.py
@@ -1,11 +1,8 @@
 import streamlit as st
 
-# 在Streamlit应用中嵌入本地视频文件
-st.title('视频播放器示例')
-
-# 提供本地视频文件路径或网络视频链接
-video_path = 'path/to/your/video.mp4'
+# 提供GitHub上的视频文件URL
+video_url = 'https://github.com/Shuying-exquisite/streamlit-app/blob/main/%E5%BE%AE%E4%BF%A1%E7%8A%B6%E6%80%81%E8%83%8C%E6%99%AFapi/video/%E5%92%8F%E6%98%A5%E5%8F%B6%E9%97%AE.mp4?raw=true'
 
 # 使用 st.video() 播放视频
-st.video(video_path)
-
+st.title('播放视频示例')
+st.video(video_url)
