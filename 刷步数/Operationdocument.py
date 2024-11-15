bb53fd421d80b336bf423a9111e49a651ee38cab
ShuyinYang <103890056+Shuying-exquisite@users.noreply.github.com>
2024-11-16 00:57:10 +0800

    Update Operationdocument.py

--- 刷步数/Operationdocument.py
+++ 刷步数/Operationdocument.py
@@ -2,17 +2,12 @@ import streamlit as st
 import re
 import os
 
-folder_path = "/mount/src/streamlit-app/刷步数"  # 指定文件夹路径
 
-# 获取文件夹下的所有文件和子目录
-files = os.listdir(folder_path)
-
-# 打印文件和子目录列表
-for file in files:
-    st.write(file)
 
 # 读取 Markdown 文件
-with open("Operationdocument.md", "r", encoding="utf-8") as file:
+with open("/mount/src/streamlit-app/刷步数/Operationdocument.md", "r", encoding="utf-8") as file:
+    # 处理文件内容
+
     markdown_text = file.read()
 
 # 在 Streamlit 应用中显示 Markdown 内容，同时处理图片
