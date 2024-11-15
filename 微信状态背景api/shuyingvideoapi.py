849dd3d8e3891d6026f7bf81b87a70cb316347d6
ShuyinYang <103890056+Shuying-exquisite@users.noreply.github.com>
2024-11-16 02:07:39 +0800

    Update shuyingvideoapi.py

--- 微信状态背景api/shuyingvideoapi.py
+++ 微信状态背景api/shuyingvideoapi.py
@@ -1,8 +1,21 @@
 import streamlit as st
+import random
+import requests
 
-# 提供GitHub上的视频文件URL
-video_url = 'https://github.com/Shuying-exquisite/streamlit-app/blob/main/微信状态背景api/video/明明就.mp4?raw=true'
+# GitHub API URL
+repo_url = "https://api.github.com/repos/Shuying-exquisite/streamlit-app/contents/微信状态背景api/video"
 
-# 使用 st.video() 播放视频
-st.title('播放视频示例')
-st.video(video_url)
+# 获取GitHub目录下的视频文件
+response = requests.get(repo_url)
+data = response.json()
+
+# 提取视频文件的原始链接（过滤出以.mp4结尾的文件）
+video_urls = [file['download_url'] for file in data if file['name'].endswith('.mp4')]
+
+# 随机选择一个视频链接
+if video_urls:
+    selected_video = random.choice(video_urls)
+    st.title('随机播放视频')
+    st.video(selected_video)
+else:
+    st.write('没有找到视频文件。')
