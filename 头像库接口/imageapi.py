d82b8e479cd9180028c9afb89478d8cb65bf2010
ShuyinYang <103890056+Shuying-exquisite@users.noreply.github.com>
2024-11-18 01:20:39 +0800

    Update imageapi.py

--- 头像库接口/imageapi.py
+++ 头像库接口/imageapi.py
@@ -1,11 +1,20 @@
 import streamlit as st
 
+# 设置页面配置
+st.set_page_config(page_title="图片展示", layout="centered", initial_sidebar_state="collapsed")
+
+# 隐藏所有的默认元素（如：标题、边框等）
+hide_st_style = """
+    <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 # 图片 URL
 image_url = "https://raw.githubusercontent.com/Shuying-exquisite/streamlit-app/main/头像库接口/image/3.jpg"
 
-# 隐藏所有页面元素
-st.set_page_config(page_title="图片展示", layout="centered")
-
-# 仅显示图片
+# 显示图片
 st.image(image_url, caption="头像图片", use_column_width=True)
-
