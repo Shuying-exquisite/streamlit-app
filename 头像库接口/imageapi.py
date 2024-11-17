ec8fafd229b1a708bad378df0931d60dd78261af
ShuyinYang <103890056+Shuying-exquisite@users.noreply.github.com>
2024-11-18 00:17:56 +0800

    Update imageapi.py

--- 头像库接口/imageapi.py
+++ 头像库接口/imageapi.py
@@ -1,1 +1,21 @@
 import streamlit as st
+import random
+import requests
+
+# GitHub API URL
+repo_url = "https://api.github.com/repos/Shuying-exquisite/streamlit-app/contents/微信状态背景api/images"
+
+# 获取GitHub目录下的图像文件
+response = requests.get(repo_url)
+data = response.json()
+
+# 提取图像文件的原始链接（过滤出常见图片格式的文件）
+image_urls = [file['download_url'] for file in data if file['name'].lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+
+# 随机选择一张图像链接
+if image_urls:
+    selected_image = random.choice(image_urls)
+    st.title('随机显示图像')
+    st.image(selected_image, use_column_width=True)
+else:
+    st.write('没有找到图像文件。')
